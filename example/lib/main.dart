d472f120facf7792f9b17f4a5386673dcdb9a273
VNNiz <contact@niz.vn>
2023-06-21 18:14:29 +0700

    Add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,1 +1,46 @@
+import 'package:app_review_helper/app_review_helper.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final instance = AppReviewHelper.instance;
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  Future<void> init() async {
+    instance.initial(
+      reviewDialogConfig: ReviewDialogConfig(
+        context: context,
+        whatCanWeDo: (comment) {
+          print(comment);
+        },
+      ),
+      minCallThisFunction: 0,
+      minDays: 0,
+      keepRemind: true,
+      isDebug: true,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Review Helper')),
+      body: const Center(child: Text('Example')),
+    );
+  }
+}
