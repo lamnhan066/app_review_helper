10feaa8e009fd083153a5c7fb23d850659645659
lamnhan066 <hello@lamnhan.dev>
2024-01-03 12:40:06 +0700

    Export review_dialog

--- lib/app_review_helper.dart
+++ lib/app_review_helper.dart
@@ -1,5 +1,6 @@
 library app_review_helper;
 
 export 'src/app_review_helper.dart';
+export 'src/models/review_dialog.dart';
 export 'src/models/review_dialog_config.dart';
 export 'src/models/review_state.dart';
